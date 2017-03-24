b1cc0fc9e54483ab5772de2c0d90565b3deeb105
misawa <s.misawa11@gmail.com>
2017-03-24 18:06:52 +0000

    peco install までinit.shで

--- init.sh
+++ init.sh
@@ -3,3 +3,9 @@ ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 rm -r ~/.vim
 ln -s ~/dotfiles/.vim ~/.vim
+
+#peco install
+cd
+wget https://github.com/peco/peco/releases/download/v0.2.0/peco_linux_amd64.tar.gz
+tar xfvz peco_linux_amd64.tar.gz
+sudo cp peco_linux_amd64/peco /usr/local/bin
