0dfe13c085fceddd8136dfaf2299712c9f5da2ec
misawa <satoshi_misawa@gochipon.co.jp>
2019-05-22 08:22:13 +0000

    人様にご迷惑をお掛けしない

--- init.sh
+++ init.sh
@@ -8,7 +8,9 @@ ln -s ~/dotfiles/.vim ~/.vim
 cd ~/dotfiles/
 wget https://github.com/peco/peco/releases/download/v0.2.0/peco_linux_amd64.tar.gz
 tar xfvz peco_linux_amd64.tar.gz
-sudo cp peco_linux_amd64/peco /usr/local/bin
+mkdir -p ~/bin
+sudo cp peco_linux_amd64/peco ~/bin
+PATH=$PATH:~/bin
 
 source ~/.zshrc
 source ~/.vimrc
